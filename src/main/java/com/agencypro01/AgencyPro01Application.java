310a9bba815b040fec3e490a9129a4089f7cbbc5
AppleLee0423 <ld210@naver.com>
2024-03-09 23:55:04 +0900

    init commit

---
+++ src/main/java/com/agencypro01/AgencyPro01Application.java
@@ -0,0 +1,13 @@
+package com.agencypro01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyPro01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgencyPro01Application.class, args);
+    }
+
+}
