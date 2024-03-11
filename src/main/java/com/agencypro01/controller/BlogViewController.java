96480443fc7dac903e8bb1dddafc89c8dbd267ef
AppleLee0423 <ld210@naver.com>
2024-03-11 23:28:15 +0900

    175pg Thymeleaf로 구현한 페이지 React로 구현하기

--- src/main/java/com/agencypro01/controller/BlogViewController.java
+++ src/main/java/com/agencypro01/controller/BlogViewController.java
@@ -1,12 +1,9 @@
 package com.agencypro01.controller;
 
-import com.agencypro01.dto.ArticleListViewResponse;
 import com.agencypro01.dto.ArticleResponse;
 import com.agencypro01.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
