efe51b80fa967a024ef52a83b80eb712f07a5a09
AppleLee0423 <ld210@naver.com>
2024-03-17 22:20:01 +0900

    Spring Security config set

---
+++ src/main/java/com/agencypro01/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.agencypro01.repository;
+
+import com.agencypro01.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/agencypro01/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.agencypro01.service;
+
+import com.agencypro01.domain.User;
+import com.agencypro01.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
